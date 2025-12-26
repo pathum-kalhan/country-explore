7aeea25b62706adda1fc3121f18985e1d715956a
pathum kalhan <pathumsimpson@gmail.com>
2025-12-26 08:35:06 +0530

    feat: implement setCookie function to handle cookie setting

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   
    <h1>hello</h1>
+   <button @click="setCookie">Set Cookie</button>
 </template>
 <script setup>
-
+const setCookie = async () => {
+  try {
+    const response = await $fetch('/api/set-cookies')
+    console.log(response)
+  } catch (error) {
+    console.error('Error setting cookie:', error)
+  }
+}
 </script>

0b3246657298ba01cd701ba4151339ac6892d01c
pathum kalhan <pathumsimpson@gmail.com>
2025-12-26 08:40:37 +0530

    feat: enhance setCookie function with additional options for security and lifespan

--- server/api/set-cookies.ts
+++ server/api/set-cookies.ts
@@ -1,5 +1,10 @@
 export default defineEventHandler((event) => {
-  setCookie(event, 'pathumToken', 'this is the cookie')
+  setCookie(event, 'pathumToken', 'this is the cookie', {
+    maxAge: 60 * 60 * 24 * 365, // 1 year in seconds
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax'
+  })
   
   return {
     success: true,
